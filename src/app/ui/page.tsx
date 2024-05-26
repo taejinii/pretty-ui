5586c6caba161e9afb4cdaf2eaa4fd9804ff5c17
taejinii <xowls177@gmail.com>
2024-05-27 01:22:58 +0900

    바텀시트 컴포넌트 구현

---
+++ src/app/ui/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import BottomSheet from "@/components/bottom-sheet/bottom-sheet";
+import { useEffect, useState } from "react";
+import { AnimatePresence } from "framer-motion";
+
+export default function UI() {
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflowY = "hidden";
+    } else {
+      document.body.style.overflowY = "auto";
+    }
+  }, [isOpen]);
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <button
+        className="bg-black w-20 text-white p-2 rounded-lg"
+        onClick={() => setIsOpen(true)}
+      >
+        Open
+      </button>
+      <AnimatePresence>
+        {isOpen && <BottomSheet onClose={() => setIsOpen(false)} />}
+      </AnimatePresence>
+    </div>
+  );
+}
---
+++ src/components/bottom-sheet/bottom-sheet.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { motion, useDragControls, useMotionValue } from "framer-motion";
+
+interface BottomSheetProps {
+  onClose: () => void;
+}
+
+function BottomSheetItem({ children }: { children: React.ReactNode }) {
+  return (
+    <button className=" active:bg-gray-300 rounded-xl p-2 w-full active:scale-90 transition-all ">
+      {children}
+    </button>
+  );
+}
+
+function Overlay({ onClose }: BottomSheetProps) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="absolute min-w-screen min-h-screen bg-black/20 inset-0 overflow-hidden"
+      onClick={onClose}
+    />
+  );
+}
+
+function BottomSheet({ onClose }: BottomSheetProps) {
+  const controls = useDragControls();
+  const dragY = useMotionValue(0);
+  console.log(dragY);
+
+  return (
+    <>
+      <Overlay onClose={onClose} />
+      <motion.div
+        className="bg-white inset-x-0 w-80  mx-auto items-center text-black p-2 fixed bottom-4 flex flex-col overflow-hidden"
+        style={{ borderRadius: 30, y: dragY }}
+        initial={{ y: 336, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 500, opacity: 0 }}
+        transition={{ duration: 0.2, ease: "easeIn" }}
+        drag="y"
+        layout
+        dragConstraints={{ top: 0, bottom: 0 }}
+        dragElastic={{ top: 0, bottom: 0.5 }}
+        dragListener={false}
+        dragControls={controls}
+        onDragEnd={() => {
+          if (dragY.get() >= 50) {
+            onClose();
+          }
+        }}
+        onPointerDown={(e) => controls.start(e)}
+      >
+        <button>
+          <hr className="w-14 h-2 bg-black rounded-xl cursor-grab active:cursor-grabbing mb-2" />
+        </button>
+        <BottomSheetItem>1</BottomSheetItem>
+        <BottomSheetItem>2</BottomSheetItem>
+        <BottomSheetItem>3</BottomSheetItem>
+        <BottomSheetItem>4</BottomSheetItem>
+      </motion.div>
+    </>
+  );
+}
+export default BottomSheet;
